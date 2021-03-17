4dba1b57ddae427dd700f87545055d219cdd50e3
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 18:49:54 +0000

    Create list-and-membership-operators.py

---
+++ data structures/list-and-membership-operators.py
@@ -0,0 +1,11 @@
+# Use list indexing to determine how many days are in a particular month based on the integer variable month, 
+# and store that value in the integer variable num_days. For example, if month is 8, num_days should be set to 31, since the eighth month, August, has 31 days
+
+month = 8
+days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+# use list indexing to determine the number of days in month
+
+month = ['January']
+num_days = 31
+print(num_days)
