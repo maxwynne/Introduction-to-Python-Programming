ba63cd9d35c23790f840d976d65576059bf5fd83
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 18:03:32 +0000

    Create server-log-message.py

---
+++ server-log-message.py
@@ -0,0 +1,8 @@
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+username = "Kinari accessed the site"
+timestamp = "at 04:50."
+url = "http://petshop.com/pets/mammals/cats"
+print(username + " " + url + " " + timestamp)
