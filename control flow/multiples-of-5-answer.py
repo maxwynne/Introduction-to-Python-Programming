86ad6188115d5e030dfdb5bae00110cf83bc756b
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 19:39:09 +0000

    Create multiples-of-5-answer.py

---
+++ control flow/multiples-of-5-answer.py
@@ -0,0 +1,18 @@
+# Write a for loop below that will print out every whole number that is a multiple of 5 and less than or equal to 30. This should output:
+
+5
+10
+15
+20
+25
+30
+
+# ANSWER
+
+# Write a for loop using range() to print out multiples of 5 up to 30 inclusive
+
+numbers = ("5", "10", "15", "20", "25", "30")
+for x in numbers:
+    if x == "0":
+      continue
+    print (x)
