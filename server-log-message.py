01343852dcb44bb5fb75d09adef1cabc27e4564d
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 18:11:56 +0000

    Update server-log-message.py

--- server-log-message.py
+++ server-log-message.py
@@ -1,3 +1,8 @@
+// Quiz: Total Sales 
+// In this quiz, you’ll need to change the types of the input and output data in order to get the result you want.
+// Calculate and print the total sales for the week from the data provided. 
+// Print out a string of the form "This week's total sales: xxx", where xxx will be the actual total of all the numbers. You’ll need to change the type of the input data in order to calculate that total.
+
 username = "Kinari"
 timestamp = "04:50"
 url = "http://petshop.com/pets/mammals/cats"
