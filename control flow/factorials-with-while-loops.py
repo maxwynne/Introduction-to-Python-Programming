11da9f9365458877d4c1fe944d50f87e4d0ef3ec
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 21:46:56 +0000

    Create factorials-with-while-loops.py

---
+++ control flow/factorials-with-while-loops.py
@@ -0,0 +1,31 @@
+#Find the factorial of a number using a while loop
+
+# number to find the factorial of
+number = 6   
+
+# start with our product equal to one
+product = 1
+
+# track the current number being multiplied
+current = 1
+
+# write your while loop here
+
+number = 6 #number to find factorial of
+product = 1 #start with product
+current = 1 #track number being multiplied
+
+while current <= number:
+    product *= current
+    current += 1
+    
+print(product)
+
+    # multiply the product so far by the current number
+    
+    
+    # increment current with each iteration until it reaches number
+
+
+
+# print the factorial of number
