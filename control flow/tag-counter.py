738030d6d877521fa496db4b12429aa3550fc896
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 21:36:46 +0000

    Create tag-counter.py

---
+++ control flow/tag-counter.py
@@ -0,0 +1,14 @@
+# Write a for loop that iterates over a list of strings, tokens, and counts how many of them are XML tags. 
+# XML is a data language similar to HTML.
+# You can assume that the list of strings will not contain empty strings.
+
+tokens = ['<greeting>', 'Hello World!', '</greeting>']
+count = 2
+
+# write your for loop here
+
+tokens = ["<greeting>", "</greeting>"]
+for x in tokens:
+    print (x)
+
+print(count)
