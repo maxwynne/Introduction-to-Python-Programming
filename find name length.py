50a749e6a76ab1e3d76fdd6170656e36e3bfa1b1
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 18:04:08 +0000

    Create find name length.py

---
+++ find name length.py
@@ -0,0 +1,8 @@
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+
+name_length = len ("William" + " " + "Bradley" + " " + "Pitt")
+
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)
