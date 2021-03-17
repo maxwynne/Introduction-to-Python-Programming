d244330ae7ed57f83c98e6e5b6d5cac3e340adfa
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 21:40:36 +0000

    Create iterating-through-dictionaries (fruit basket task 2).py

---
+++ control flow/iterating-through-dictionaries (fruit basket task 2).py
@@ -0,0 +1,32 @@
+# If your solution is robust, you should be able to use it with any dictionary of items to count the number of fruits in the basket.
+# Try the loop for each of the dictionaries below to make sure it always works.
+
+result = 0
+basket_items = {'pears': 5, 'grapes': 19, 'kites': 3, 'sandwiches': 8, 'bananas': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for object, count in basket_items.items():
+    if object in fruits:
+        result += count
+
+print(result)
+
+result = 0
+basket_items = {'peaches': 5, 'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for object, count in basket_items.items():
+    if object in fruits:
+        result += count
+
+print(result)
+
+result = 0
+basket_items = {'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4, 'bears': 10}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for object, count in basket_items.items():
+    if object in fruits:
+        result += count
+
+print(result)
