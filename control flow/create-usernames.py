1c22b4209a2f416048003d0769f9f324f56145be
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-17 21:35:45 +0000

    Create create-usernames.py

---
+++ control flow/create-usernames.py
@@ -0,0 +1,19 @@
+# Write a for loop that iterates over the names list to create a usernames list. To create a username for each name, make everything lowercase and replace spaces with underscores
+
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+usernames = ["joey_tribbiani", "monica_geller", "chandler_bing", "phoebe_buffay"]
+
+# write your for loop here
+
+for x in names:
+    print(x)
+
+print(usernames)
+      
+# Output
+      
+Joey Tribbiani
+Monica Geller
+Chandler Bing
+Phoebe Buffay
+['joey_tribbiani', 'monica_geller', 'chandler_bing', 'phoebe_buffay']
